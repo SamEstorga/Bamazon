fe32dfb0dce67629b155ba05294b492e42bb11fd
Sam Estorga <estorgajr@gmail.com>
2019-08-20 18:36:50 -0700

    customer.js

---
+++ bamazon.js
@@ -0,0 +1,65 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'cobyrvca7',
+    database: 'bamazon'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connection Successful");
+    makeTable();
+    
+})
+
+let makeTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].itemid + " || " + res[i].productname + " || " + res[i].departmentname + " || " + res[i].price + " || " + res[i].stockquantity + "\n");
+        }
+    promptCustomer(res);
+    })
+}
+
+let promptCustomer = function(res) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: 'What would you like to purchase?'
+    }]).then (function(answer) {
+        let correct = false;
+        for(let i = 0; i < res.length; i++) {
+            if (res[i].productname == answer.choice) {
+                correct = true;
+                let product = answer.choice;
+                let id = i;
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'quantity',
+                    message: 'How many would you like to buy?',
+                    validate: function(value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then (function(answer) {
+                    if((res[id].stockquantity-answer.quantity) > 0) {
+                        connection.query("UPDATE products SET stockquantity = '" + (res[id].stockquantity-answer.quantity) + "' WHERE productname= '" + product + "'", function(err,res2) {
+                            console.log("Product Purchased");
+                            makeTable();
+                        })
+                    } else {
+                        console.log("Not a valid selection");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+    })
+}
